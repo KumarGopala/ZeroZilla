df2154cab91a426777c5f2d35b58e32346abd509
Amit Naik <amit.naik8103@gmail.com>
2018-03-10 16:44:58 +0530

    added services to share the data

---
+++ AngularJSAuthentication.Web/app/services/sharedPropertiesService.js
@@ -0,0 +1,14 @@
+﻿'use strict';
+app.service('sharedProperties', function () {
+
+    var hashtable = {};
+
+    return {
+        setValue: function (key, value) {
+            hashtable[key] = value;
+        },
+        getValue: function (key) {
+            return hashtable[key];
+        }
+    }
+});
