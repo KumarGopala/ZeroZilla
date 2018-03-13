48fe9c2fde85637d28a9b2e5f8d2caf259cd5531
Amit Naik <amit.naik8103@gmail.com>
2018-03-13 06:19:46 +0530

    Added file upload feature

--- AngularJSAuthentication.API/Controllers/PriceQuoteController.cs
+++ AngularJSAuthentication.API/Controllers/PriceQuoteController.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
+using System.Web;
 using System.Web.Http;
+using ZeroZilla.API.Extension;
+using ZeroZilla.API.Models;
 using ZeroZilla.API.Repository;
 
 namespace ZeroZilla.API.Controllers
@@ -30,5 +34,130 @@ namespace ZeroZilla.API.Controllers
            var response= await _repo.GetPriceQuote(wordCount, deliveryType);
             return Ok(response);
         }
+
+
+
+        private readonly string workingFolder = HttpRuntime.AppDomainAppPath + @"\Uploads";
+
+        /// <summary>
+        ///   Get all docs
+        /// </summary>
+        /// <returns></returns>
+        public async Task<IHttpActionResult> Get()
+        {
+            var docs = new List<DocViewModel>();
+
+            var docFolder = new DirectoryInfo(workingFolder);
+
+            await Task.Factory.StartNew(() =>
+            {
+                docs = docFolder.EnumerateFiles()
+                    .Where(fi => new[] { ".jpeg", ".jpg", ".bmp", ".png", ".gif", ".tiff" }
+                        .Contains(fi.Extension.ToLower()))
+                    .Select(fi => new DocViewModel
+                    {
+                        Name = fi.Name,
+                        Created = fi.CreationTime,
+                        Modified = fi.LastWriteTime,
+                        Size = fi.Length / 1024
+                    })
+                    .ToList();
+            });
+
+            return Ok(new { docs = docs });
+        }
+
+        /// <summary>
+        ///   Delete doc
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        [HttpDelete]
+        public async Task<IHttpActionResult> Delete(string fileName)
+        {
+            if (!FileExists(fileName))
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                var filePath = Directory.GetFiles(workingFolder, fileName)
+                    .FirstOrDefault();
+
+                await Task.Factory.StartNew(() =>
+                {
+                    if (filePath != null)
+                        File.Delete(filePath);
+                });
+
+                var result = new DocActionResult
+                {
+                    Successful = true,
+                    Message = fileName + "deleted successfully"
+                };
+                return Ok(new { message = result.Message });
+            }
+            catch (Exception ex)
+            {
+                var result = new DocActionResult
+                {
+                    Successful = false,
+                    Message = "error deleting fileName " + ex.GetBaseException().Message
+                };
+                return BadRequest(result.Message);
+            }
+        }
+
+        
+        [Route("files")]
+        public async Task<IHttpActionResult> Add()
+        {
+            // Check if the request contains multipart/form-data.
+            if (!Request.Content.IsMimeMultipartContent("form-data"))
+            {
+                return BadRequest("Unsupported media type");
+            }
+            try
+            {
+                var provider = new CustomMultipartFormDataStreamProvider(workingFolder);
+                //await Request.Content.ReadAsMultipartAsync(provider);
+                await Task.Run(async () => await Request.Content.ReadAsMultipartAsync(provider));
+
+                var docs = new List<DocViewModel>();
+
+                foreach (var file in provider.FileData)
+                {
+                    var fileInfo = new FileInfo(file.LocalFileName);
+
+                    docs.Add(new DocViewModel
+                    {
+                        Name = fileInfo.Name,
+                        Created = fileInfo.CreationTime,
+                        Modified = fileInfo.LastWriteTime,
+                        Size = fileInfo.Length / 1024
+                    });
+                }
+                return Ok(new { Message = "Doc uploaded ok", docs = docs });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.GetBaseException().Message);
+            }
+        }
+
+        /// <summary>
+        ///   Check if file exists on disk
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        public bool FileExists(string fileName)
+        {
+            var file = Directory.GetFiles(workingFolder, fileName)
+                .FirstOrDefault();
+
+            return file != null;
+        }
+
     }
 }
--- AngularJSAuthentication.Web/app/controllers/pricingController.js
+++ AngularJSAuthentication.Web/app/controllers/pricingController.js
@@ -1,72 +1,95 @@
 ﻿'use strict';
-app.controller('pricingController', ['$rootScope', '$scope', 'pricingService', '$http', 'ngAuthSettings', 'authService', '$location', function ($rootScope,$scope, pricingService, $http, ngAuthSettings, authService, $location) {
-    debugger;
+app.controller('pricingController', ['$rootScope', '$scope', 'pricingService', '$http', 'ngAuthSettings', 'authService', '$location',
+    "$window", "Upload", //"apiUrl",
+    function ($rootScope, $scope, pricingService, $http, ngAuthSettings, authService, $location, $window, Upload) {
 
-    var cnt = $scope.wordCount;
-    $scope.pricing = [];
-
-    var serviceBase = ngAuthSettings.apiServiceBaseUri;
-    $scope.GetPrice = function (wordCount, deliveryType) {
-      
-        //$http.get(serviceBase + '/api/PriceQuote/price/' + wordCount + '/' + deliveryType).then(function (results) {
-        //    debugger;
-        //    return results.data;
-        //}, function (error) {
-
-        //    debugger;
-        //    });
-        $http.get(serviceBase + 'api/PriceQuote/price/' + wordCount + '/' + deliveryType).then(function (results) {
-            debugger;
-            $scope.PriceQuote = results.data;
-        }, function (error) {
-
-            debugger;
-        });
-
-    }
-    
-    $scope.files = [];
-
-    //listen for the file selected event
-    $scope.$on("fileSelected", function (event, args) {
-        $scope.$apply(function () {
-            //add the file object to the scope's files collection
-            $scope.files.push(args.file);
-        });
-    });
-
-    //https://shazwazza.com/post/uploading-files-and-json-data-in-the-same-request-with-angular-js/
-    //https://stackoverflow.com/questions/31987723/unable-to-upload-doc-or-docx-file
-    
-    
-    $scope.PlaceOrder = function () {
-
+        var cnt = $scope.wordCount;
         $scope.pricing = [];
-        $scope.pricing.wordCount = $scope.wordCount;
-        $scope.pricing.deliveryType = $scope.deliveryType;
-        $scope.pricing.PriceQuote = $scope.PriceQuote;
 
-        $rootScope.pricing = $scope.pricing;
+        var serviceBase = ngAuthSettings.apiServiceBaseUri;
+        $scope.GetPrice = function (wordCount, deliveryType) {
 
-        debugger;
-        if (!authService.authentication.isAuth) {
-            $location.path('/signin');
+            //$http.get(serviceBase + '/api/PriceQuote/price/' + wordCount + '/' + deliveryType).then(function (results) {
+            //    debugger;
+            //    return results.data;
+            //}, function (error) {
+
+            //    debugger;
+            //    });
+            $http.get(serviceBase + 'api/PriceQuote/price/' + wordCount + '/' + deliveryType).then(function (results) {
+                debugger;
+                $scope.PriceQuote = results.data;
+            }, function (error) {
+
+                debugger;
+            });
+
         }
-        else {
-            $location.path('/orderRequirement');
+
+        $scope.files = [];
+
+        //listen for the file selected event
+        $scope.$on("fileSelected", function (event, args) {
+            $scope.$apply(function () {
+                //add the file object to the scope's files collection
+                $scope.files.push(args.file);
+            });
+        });
+
+        //https://shazwazza.com/post/uploading-files-and-json-data-in-the-same-request-with-angular-js/
+        //https://stackoverflow.com/questions/31987723/unable-to-upload-doc-or-docx-file
+
+
+        $scope.PlaceOrder = function () {
+
+            $scope.pricing = [];
+            $scope.pricing.wordCount = $scope.wordCount;
+            $scope.pricing.deliveryType = $scope.deliveryType;
+            $scope.pricing.PriceQuote = $scope.PriceQuote;
+
+            $rootScope.pricing = $scope.pricing;
+
+            debugger;
+            if (!authService.authentication.isAuth) {
+                $location.path('/signin');
+            }
+            else {
+                $location.path('/orderRequirement');
+            }
+
         }
-        
-    }
 
 
-    $scope.Get2 = pricingService.getPrice(1, 1).then(function (results) {
-        debugger;
-        alert(10);
-        $scope.Amount = results.data;
+        $scope.Get2 = pricingService.getPrice(1, 1).then(function (results) {
+            debugger;
+            alert(10);
+            $scope.Amount = results.data;
 
-    }, function (error) {
-        //alert(error.data.message);
+        }, function (error) {
+            //alert(error.data.message);
         });
 
 
-}]);
+
+        $scope.uploadFiles =   function(files) {
+            debugger;
+            //vm.spinner.active = true;
+            Upload.upload({
+                url: serviceBase +"api/PriceQuote/files",
+                data: { file: files }
+            })
+                .then(function (response) {
+                    //activate();
+                    //setPreviewPhoto();
+                    //vm.spinner.active = false;
+                }, function (err) {
+                    console.log("Error status: " + err.status);
+                    //vm.spinner.active = false;
+                });
+
+
+        }
+
+
+    }]);
+ 
